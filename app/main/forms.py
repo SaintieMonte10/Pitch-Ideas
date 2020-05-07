8665c0764c3f85ad948f1fb529a2522dabc8cd27
SaintieMonte10 <claremonte53@gmai.com>
2020-05-07 23:34:36 +0300

    imports on views.py

---
+++ app/main/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField #SelectField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    pitch = TextAreaField('Write Your Pitch',validators=[Required()])
+    pitch_category = SelectField('Pitch Category',choices=[('Technology-Pitch','Technology Pitch'),('Business-Pitch','Business Pitch'),('Interview-Pitch','Interview Pitch'),('Pickup-Line','Pickup-Line Pitch'),('Promotion-Pitch','Promotion Pitch')],validators=[Required()])
+    submit = SubmitField('Submit Pitch')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment about the above Pitch', validators=[Required()])
+    submit = SubmitField('Submit Comment')
