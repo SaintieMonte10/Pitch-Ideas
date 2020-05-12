691c99656c3b9bb98add7f3cf97f5a9ce2befbc2
SaintieMonte10 <claremonte53@gmai.com>
2020-05-12 12:07:01 +0300

    final touches

---
+++ app/static/main.js
@@ -0,0 +1,10 @@
+let click = 0;
+let clicks = 0;
+function onClick() {
+    click += 1;
+    document.getElementById("click").innerHTML = click;
+}
+function onClicks() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
--- config.py
+++ config.py
@@ -4,9 +4,7 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clares:access@localhost/blog'
-
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    # Gmail Configs
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
